b94747021d9047e4d1d6c2bfb49563411307c188
Jason <mickeu420@gmail.com>
2018-10-02 11:54:45 -0400

    dsjklafsdjilfjasdklfp

--- construction.c
+++ construction.c
@@ -1,11 +1,30 @@
-#include <stdio.lib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct dog{
+struct Dog{
   int age;
-  int feet;
   char * name;
 };
 
-main(){
+struct Dog_tuple{
+  struct Dog * pointer;
+  struct Dog doggy;
+};
+
+char * letters = "qwertyuiopasdfghjklzxcvbn";
+
+char pickRand(){
+  return letters[rand()%26];
+}
   
+struct Dog makeDog(){
+  struct Dog {(rand()%18 + 2); strcat(pickRand(), "ammy")}Doggy;
+}
+
+int main(){
+  struct Dog doggy  = makeDog();
+  struct Dog * doggy_pointer = &doggy;
+  printf("%s", doggy_pointer->name);
+  return 0;
 }
---
+++ construction.c~
@@ -0,0 +1,11 @@
+#include <stdio.lib>
+
+struct dog{
+  int age;
+  int feet;
+  char * name;
+};
+
+main(){
+  
+}
